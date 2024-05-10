68f8e0269c9263a6f1e40e40de2f19c963aeca4f
yoonhc <hcyoon2000@outlook.com>
2024-05-10 21:58:09 +0900

    configured CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    exposedHeaders: ['Authorization'],
+    maxAge: 3600,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   // Swagger Setup
