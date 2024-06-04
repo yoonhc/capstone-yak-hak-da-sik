3993a701b522355227c6207897d5f07d3814d187
yoonhc <hcyoon2000@outlook.com>
2024-06-04 13:31:55 +0900

    imported medref typeormmodule in Medref module

--- src/med-refs/med-refs.module.ts
+++ src/med-refs/med-refs.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MedRefsService } from './med-refs.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MedRef } from './med-ref.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([MedRef])
+  ],
   providers: [MedRefsService]
 })
 export class MedRefsModule {}
