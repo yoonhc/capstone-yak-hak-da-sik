9b18285990c9d48076c3c73c940720f59cf6d451
yoonhc <hcyoon2000@outlook.com>
2024-06-05 14:57:25 +0900

    modified dur data(removed '병용금기' column)

--- src/durs/dur-entity.ts
+++ src/durs/dur-entity.ts
@@ -1,7 +1,36 @@
-import { Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 
 @Entity('durs')
 export class DUR {
   @PrimaryColumn({ type: 'int', width: 9 })
   id: number;
+
+  @Column({
+    type: 'enum',
+    enum: ['단일', '복합'],
+    nullable: false,
+  })
+  durCombinationType: string;             // 단일/복합
+
+  @Column({ type: 'varchar', length: 7, nullable: false })
+  durCode: string;                        // DUR성분코드	
+
+  @Column({ type: 'varchar', length: 63, nullable: false })
+  combinationDUR: string;                 // 복합제
+
+  @Column({
+    type: 'enum',
+    enum: ['단일', '복합'],
+    nullable: false,
+  })
+  contraindicateCombinationType: string;  // 병용금기단일/복합
+
+  @Column({ type: 'varchar', length: 7, nullable: false })
+  contraindicateDUR: string;              // 병용금기DUR성분코드
+
+  @Column({ type: 'varchar', length: 63, nullable: false })
+  contraindicateCombinationDUR: string;   // 병용금기복합제
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  contraindicateReason: string;           // 병용금기내용/이유
 }
--- src/durs/durs.module.ts
+++ src/durs/durs.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DursService } from './durs.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DUR } from './dur-entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([DUR])
+  ],
   providers: [DursService]
 })
 export class DursModule {}
